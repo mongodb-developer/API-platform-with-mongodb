75026e509a2ad968e5e0b53bff81803d1608ab8f
Aasawari <aasawari.sahasrabuddhe@mongodb.com>
2024-10-23 22:43:50 +0530

    Adding code for CRUD operations

---
+++ api/src/Document/Restaurant.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Document;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Document;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Field;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Id;
+
+#[ApiResource]
+#[Document]
+class Restaurant
+{
+    #[Id]
+    public string $id;
+
+    #[Field]
+    public string $name;
+
+    #[Field]
+    public string $address;
+
+    #[Field]
+    public string $borough;
+
+    #[Field]
+    public string $cusine;
+   
+    public function __construct()
+    {}
+
+public function getId(): string
+{
+    return $this->id;
+}
+public function getName(): string
+{
+    return $this->name;
+}
+public function getAddress(): string
+{
+    return $this->address;
+
+}
+public function getborough(): string
+{
+    return $this->borough;
+}
+public function getcusine(): string
+{
+    return $this->cusine;
+
+}
+public function setId($id): void {
+    $this->id = $id;
+}
+public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function setborough(string $borough): void
+    {
+        $this->borough = $borough;
+    }
+    public function setcusine(string $cusine): void
+    {
+        $this->cusine = $cusine;
+    }
+    
+}
+
